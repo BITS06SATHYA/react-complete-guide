9a4633f4161f480aee3f11fe5b6056add86a308c
sathya <neelasathya06@gmail.com>
2021-06-18 15:55:53 +0530

    commit 2 Expense COmponent Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Expenses from './components/Expenses/Expenses'
 
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 20),
+    },
+    {
+      title: 'TV',
+      amount: 199.99,
+      date: new Date(2021, 3, 31),
+    },
+    {
+      title: 'House',
+      amount: 894.67,
+      date: new Date(2021, 5, 30),
+    },
+    {
+      title: 'Airplane',
+      amount: 10994.67,
+      date: new Date(2021, 6, 20),
+    },
+  ]
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, 'Lets get started'),
+  //   React.createElement(Expenses, { items: expenses })
+  // )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}></Expenses>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
